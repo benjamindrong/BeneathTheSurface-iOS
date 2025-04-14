16243e840ffa476c3f9256b13d020107a94275cc
benjamindrong <38218923+benjamindrong@users.noreply.github.com>
2025-04-14 13:20:21 -0500

    Initial Commit

---
+++ BeneathTheSurface/BeneathTheSurfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeneathTheSurfaceApp.swift
+//  BeneathTheSurface
+//
+//  Created by Benjamin Drong on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct BeneathTheSurfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
