1989504131911de3329132930b440696ad865505
benjamindrong <38218923+benjamindrong@users.noreply.github.com>
2025-04-19 10:03:11 -0500

    api call, date pickers and get history button

---
+++ BeneathTheSurface/UI/OnThisDay/OnThisDayFormView.swift
@@ -0,0 +1,18 @@
+//
+//  OnThisDayFormView.swift
+//  BeneathTheSurface
+//
+//  Created by Benjamin Drong on 4/19/25.
+//
+
+import SwiftUI
+
+struct OnThisDayFormView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnThisDayFormView()
+}
