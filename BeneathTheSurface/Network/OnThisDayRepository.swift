9638cb47ec88ce8128c50e0d08d341399617e5bd
benjamindrong <38218923+benjamindrong@users.noreply.github.com>
2025-04-19 10:16:12 -0500

    checks in files that were not staged in the last commit
    (cherry picked from commit 3550e8dd77c99ee22dd4379c7aaf95b5e0dfe97a)

--- BeneathTheSurface/Network/OnThisDayRepository.swift
+++ BeneathTheSurface/Network/OnThisDayRepository.swift
@@ -1,8 +1,25 @@
-//
-//  OnThisDayRepository.swift
-//  BeneathTheSurface
+// OnThisDayRepository.swift
+// BeneathTheSurface
 //
 //  Created by Benjamin Drong on 4/19/25.
 //
 
 import Foundation
+import Combine
+
+class OnThisDayRepository {
+
+    private let baseURL = "https://en.wikipedia.org/api/rest_v1/feed/onthisday/selected"
+
+    func fetchOnThisDayData(month: Int, day: Int) -> AnyPublisher<OnThisDayData, Error> {
+        let urlString = "\(baseURL)/\(month)/\(day)"
+        guard let url = URL(string: urlString) else {
+            return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
+        }
+
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: OnThisDayData.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
